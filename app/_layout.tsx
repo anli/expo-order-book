a88c7060ea1493d8139d32a839ba2f1100371d2a
Su Anli <anli@Sus-MacBook-Pro.local>
2024-09-09 14:20:46 +0800

    feat: add error boundary

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,34 @@
-import { Stack } from 'expo-router';
+import { ErrorBoundaryProps, Stack } from 'expo-router';
 import { SessionProvider } from '@/entities/session';
 import tw, { useDeviceContext } from 'twrnc';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { DeviceProvider } from '@/shared/lib';
+import { View } from 'react-native';
+import { Button, Text } from 'react-native-paper';
 
 const queryClient = new QueryClient();
+
+export function ErrorBoundary({ error, retry }: ErrorBoundaryProps) {
+  useDeviceContext(tw);
+
+  return (
+    <View style={tw`flex-1 justify-center items-center p-4 bg-red-100`}>
+      <Text style={tw`text-xl font-bold mb-4 text-red-700`}>
+        Oops! Something went wrong.
+      </Text>
+      <Text style={tw`text-base mb-6 text-center text-red-600`}>
+        {error.message}
+      </Text>
+      <Button
+        mode="contained"
+        onPress={retry}
+        style={tw`text-lg font-semibold text-blue-600 underline`}>
+        Try Again
+      </Button>
+    </View>
+  );
+}
 
 export default function Root() {
   useDeviceContext(tw);
